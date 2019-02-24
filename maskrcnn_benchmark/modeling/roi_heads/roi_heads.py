4b86d8e19a2135e116f07a561a61a81b0b62c759
pzzhang <pzzhang@caltech.edu>
2019-02-23 17:27:35 -0800

    Revert "add attribute head"
    This reverts commit dbe33f42483f2c3254efb8b70fb1153c78d21128.

--- maskrcnn_benchmark/modeling/roi_heads/roi_heads.py
+++ maskrcnn_benchmark/modeling/roi_heads/roi_heads.py
@@ -2,8 +2,6 @@
 import torch
 
 from .box_head.box_head import build_roi_box_head
-## TO DO
-from .attribute_head.attribute_head import build_roi_attribute_head
 from .mask_head.mask_head import build_roi_mask_head
 from .keypoint_head.keypoint_head import build_roi_keypoint_head
 
@@ -27,15 +25,6 @@ class CombinedROIHeads(torch.nn.ModuleDict):
         # TODO rename x to roi_box_features, if it doesn't increase memory consumption
         x, detections, loss_box = self.box(features, proposals, targets)
         losses.update(loss_box)
-
-        if self.cfg.ATTRIBUTE_ON:
-            # the attribute head reuse the features from the box head
-            attribute_features = x
-            # During training, self.box() will return the unaltered proposals as "detections"
-            # this makes the API consistent during training and testing
-            x_attr, detections, loss_mask = self.attribute(mask_features, detections, targets)
-            losses.update(loss_mask)
-
         if self.cfg.MODEL.MASK_ON:
             mask_features = features
             # optimization: during training, if we share the feature extractor between
@@ -47,13 +36,13 @@ class CombinedROIHeads(torch.nn.ModuleDict):
                 mask_features = x
             # During training, self.box() will return the unaltered proposals as "detections"
             # this makes the API consistent during training and testing
-            x_mask, detections, loss_mask = self.mask(mask_features, detections, targets)
+            x, detections, loss_mask = self.mask(mask_features, detections, targets)
             losses.update(loss_mask)
 
         if self.cfg.MODEL.KEYPOINT_ON:
             keypoint_features = features
             # optimization: during training, if we share the feature extractor between
-            # the box and the keypoint heads, then we can reuse the features already computed
+            # the box and the mask heads, then we can reuse the features already computed
             if (
                 self.training
                 and self.cfg.MODEL.ROI_KEYPOINT_HEAD.SHARE_BOX_FEATURE_EXTRACTOR
@@ -61,7 +50,7 @@ class CombinedROIHeads(torch.nn.ModuleDict):
                 keypoint_features = x
             # During training, self.box() will return the unaltered proposals as "detections"
             # this makes the API consistent during training and testing
-            x_keypoint, detections, loss_keypoint = self.keypoint(keypoint_features, detections, targets)
+            x, detections, loss_keypoint = self.keypoint(keypoint_features, detections, targets)
             losses.update(loss_keypoint)
         return x, detections, losses
 
@@ -75,8 +64,6 @@ def build_roi_heads(cfg, in_channels):
 
     if not cfg.MODEL.RPN_ONLY:
         roi_heads.append(("box", build_roi_box_head(cfg, in_channels)))
-    if cfg.MODEL.ATTRIBUTE_ON:
-        roi_heads.append(("attribute", build_roi_attribute_head(cfg, in_channels)))
     if cfg.MODEL.MASK_ON:
         roi_heads.append(("mask", build_roi_mask_head(cfg, in_channels)))
     if cfg.MODEL.KEYPOINT_ON:
